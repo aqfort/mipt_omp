7e67baed080d0a64ca01e67bab03a4b6e6b5e1af
aqfort <valiev1379@gmail.com>
2020-09-22 19:10:46 +0300

    + optimal indices + parallel

--- matrix.cpp
+++ matrix.cpp
@@ -8,12 +8,28 @@
 #include <stdio.h>
 #include <omp.h>
 
+// ok, size = 1000
+
+// ijk: 6.2467250000
+// ikj: 4.2248852000 --- optimal
+// jik: 4.3990838000
+// jki: 8.0499976000
+// kij: 5.3461839000
+// kji: 7.8837998000
+
 using namespace std;
 
 void get_matrix_size_from_cin(int& number);
 int parse_int_from_string(const string& input);
 void init_matrix(const int& size, int** A);
 void print_matrix(const int& size, int** matrix);
+void reset_matrix(const int& size, int** matrix);
+
+void algorithm_1(const int& size, int** A, int** B, int** C);   // dumb
+void algorithm_2(const int& size, int** A, int** B, int** C);   // parallel
+void algorithm_3(const int& size, int** A, int** B, int** C);   // reordered dumb
+void algorithm_4(const int& size, int** A, int** B, int** C);   // reordered parallel
+void algorithm_5(const int& size, int** A, int** B, int** C);   // strassen
 
 default_random_engine generator;
 uniform_int_distribution<int> distribution(-100, 100);
@@ -26,7 +42,10 @@ int main()
     double time_1, time_2;
 
     cout << "number of threads is in range [1, " << omp_get_max_threads() << "]" << endl << 
-        "let\'s use max number of threads" << endl << endl;
+        "let\'s use max number of threads" << endl <<
+        "stopwatch accuracy = " << fixed << setprecision(7) << omp_get_wtick() << " sec" << endl << endl;
+    cout.unsetf(ios::fixed | ios::scientific);
+    cout.precision(6);
 
     omp_set_num_threads(omp_get_max_threads());
 
@@ -55,71 +74,70 @@ int main()
     init_matrix(size, A);
     init_matrix(size, B);
 
-    {
-        cout << "let\'s look at A and B:" << endl;
-        print_matrix(size, A);
-        cout << endl;
-        print_matrix(size, B);
-    }
+    // {
+    //     cout << "let\'s look at A and B:" << endl;
+    //     print_matrix(size, A);
+    //     cout << endl;
+    //     print_matrix(size, B);
+    // }
 
-    cout << endl << "algorithm" << endl;
-
-
+    // cout << endl << "algorithm" << endl;
 
 
+
+
+
+    reset_matrix(size, C);
 
     time_1 = omp_get_wtime();
-
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            C[i][j] = 0;
-            for (k = 0; k < size; k++)
-            {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-
+    algorithm_1(size, A, B, C);
     time_2 = omp_get_wtime();
 
-    cout << "dumb algorithm spent " << fixed << setprecision(10) << time_2 - time_1 << endl;
+    cout << "dumb algorithm spent " << fixed << setprecision(7) << time_2 - time_1 << endl;
     cout.unsetf(ios::fixed | ios::scientific);
     cout.precision(6);
 
-    cout << endl << "result:" << endl;
-    print_matrix(size, C);
 
 
 
 
+    reset_matrix(size, C);
 
     time_1 = omp_get_wtime();
-
-    #pragma omp parallel for    \
-    shared(A, B, C)             \
-    private(i, j, k)
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            C[i][j] = 0;
-            for (k = 0; k < size; k++)
-            {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-
+    algorithm_2(size, A, B, C);
     time_2 = omp_get_wtime();
 
-    cout << "parallel algorithm spent " << fixed << setprecision(10) << time_2 - time_1 << endl;
+    cout << "dumb parallel algorithm spent " << fixed << setprecision(7) << time_2 - time_1 << endl;
     cout.unsetf(ios::fixed | ios::scientific);
     cout.precision(6);
 
-    cout << endl << "result:" << endl;
-    print_matrix(size, C);
+
+
+
+
+    reset_matrix(size, C);
+
+    time_1 = omp_get_wtime();
+    algorithm_3(size, A, B, C);
+    time_2 = omp_get_wtime();
+
+    cout << "reordered algorithm spent " << fixed << setprecision(7) << time_2 - time_1 << endl;
+    cout.unsetf(ios::fixed | ios::scientific);
+    cout.precision(6);
+
+
+
+
+
+    reset_matrix(size, C);
+
+    time_1 = omp_get_wtime();
+    algorithm_4(size, A, B, C);
+    time_2 = omp_get_wtime();
+
+    cout << "reordered parallel algorithm spent " << fixed << setprecision(7) << time_2 - time_1 << endl;
+    cout.unsetf(ios::fixed | ios::scientific);
+    cout.precision(6);
 
 
 
@@ -236,4 +254,96 @@ void print_matrix(const int& size, int** matrix)
 
         cout << endl;
     }
+}
+
+void reset_matrix(const int& size, int** matrix)
+{
+    int i, j;
+
+    #pragma omp parallel for    \
+    private(i, j)               \
+    shared(matrix)              \
+    schedule(guided)
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            matrix[i][j] = 0;
+        }
+    }
+}
+
+void algorithm_1(const int& size, int** A, int** B, int** C)
+{
+    int i, j, k;
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            for (k = 0; k < size; k++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void algorithm_2(const int& size, int** A, int** B, int** C)
+{
+    int i, j, k;
+
+    #pragma omp parallel for    \
+    shared(A, B, C)             \
+    private(i, j, k)
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            for (k = 0; k < size; k++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void algorithm_3(const int& size, int** A, int** B, int** C)
+{
+    int i, j, k;
+
+    for (i = 0; i < size; i++)
+    {
+        for (k = 0; k < size; k++)
+        {
+            for (j = 0; j < size; j++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void algorithm_4(const int& size, int** A, int** B, int** C)
+{
+    int i, j, k;
+
+    #pragma omp parallel for    \
+    shared(A, B, C)             \
+    private(i, j, k)
+    for (i = 0; i < size; i++)
+    {
+        for (k = 0; k < size; k++)
+        {
+            for (j = 0; j < size; j++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void algorithm_5(const int& size, int** A, int** B, int** C)
+{
+
 }
