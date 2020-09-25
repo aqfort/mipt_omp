42ef85969200478aaa3bf6e8e76a3cc9abbb250a
aqfort <valiev1379@gmail.com>
2020-09-25 14:03:13 +0300

    fixed research

--- extra/job.sh
+++ extra/job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# for ((i = 50; i <= 500; i += 10))
-# do
-# ./a.out $i
-# done
+for ((i = 50; i <= 500; i += 10))
+do
+./a.out $i
+done
 
-# for ((i = 550; i <= 1000; i += 50))
-# do
-# ./a.out $i
-# done
+for ((i = 550; i <= 1000; i += 50))
+do
+./a.out $i
+done
 
 ./a.out 1500
 ./a.out 2000
